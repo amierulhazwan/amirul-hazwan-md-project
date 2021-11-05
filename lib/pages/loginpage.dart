0d335ea90b428a83a3bc6ca6b7c41c0d02b64054
Amirul Hazwan <amirulhazwan1997@gmail.com>
2021-11-05 17:17:25 +0800

    Project Update

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-// import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/io.dart';
 import '../home.dart';
 
 class LoginPage extends StatefulWidget {
@@ -13,21 +14,46 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // final _trialWebSocket =
-  //     WebSocketChannel.connect(Uri.parse('ws://besquare-demo.herokuapp.com'));
+// ------------------------------------------------------------
+  final channel =
+      IOWebSocketChannel.connect(Uri.parse('ws://besquare-demo.herokuapp.com'));
+  void listenStream() {
+    channel.stream.listen((message) {
+      final decodedMessage = jsonDecode(message);
+    });
+  }
 
-  // void apiStream() {
-  //   _trialWebSocket.stream.listen((message) {
-  //     final rajaComel = jsonDecode(message);
+  void submitData() {
+    setState(() {
+      final String enteredID = idController.text;
+      channel.sink.add('{"type": "sign_in","data":{"name": "$enteredID"}}');
+      if (enteredID.isEmpty) {
+        _isOff = true;
+      } else {
+        _isOff = false;
+      }
+    });
+  }
 
-  //     print(rajaComel);
-  //   });
-  // }
-
+  final idController = TextEditingController();
+  bool _isOff = true;
+// -----------------------------------------------------------
   @override
   void initState() {
+    // -----------------------------------------------------------
+    idController.addListener(submitData);
+    // -----------------------------------------------------------
     super.initState();
   }
+
+  // -----------------------------------------------------------
+  @override
+  void dispose() {
+    idController.dispose();
+    channel.sink.close();
+    super.dispose();
+  }
+  // -----------------------------------------------------------
 
   @override
   Widget build(BuildContext context) {
